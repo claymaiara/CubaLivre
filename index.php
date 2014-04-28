4992ad3fefade573ca5547866683667f28b9308d
Clay Maiara <clay_maiara@hotmail.com>
2014-04-28 09:01:39 -0300

    Inicio Projeto

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Home</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-inverse" role="navigation">
+			<p class="navbar-text">Seja Bem Vindo</p>
+		</nav>
+		<div class="jumbotron">
+			<h1>Cuba Livre</h1>
+			<p>Vamos montar o seu drink da maneira que desejar :)</p>
+
+		</div>
+		<div class="panel panel-success">
+			<div class="panel-heading">Primeiro Escolha o Refrigerante: </div>
+			<div class="panel-body">
+			  <button type="button" class="btn btn-defaut" data-toggle="button">Coca Cola</button>
+			  <button type="button" class="btn btn-defaut" data-toggle="button">Pepsi</button>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-inverse" role="navigation"></nav>
+	</div>
+</body>
+</html>
