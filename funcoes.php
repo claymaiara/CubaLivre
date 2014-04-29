8a4207e835b2fc7e1967f4b21626286929aeb0d7
Clay Maiara <clay_maiara@hotmail.com>
2014-04-29 14:08:15 -0300

    Funções dos graus de pertinência criadas

--- funcoes.php
+++ funcoes.php
@@ -47,4 +47,108 @@ function gCocaFraca ($qtdRefri){
 
 echo gCocaFraca(57);
 
+function gPepsiForte($qtdRefri){
+
+	if($qtdRefri < 60 OR $qtdRefri >64){
+		return 0;
+	}if (60 <= $qtdRefri AND $qtdRefri <62) {
+		return 1;
+	}if (62 <= $qtdRefri AND $qtdRefri <= 64) {
+		return (64-$qtdRefri)/(64-62);
+	}
+	else return 0;
+}
+echo gPepsiForte(61) .' ';
+
+function gPepsiSuave ($qtdRefri){
+	if ($qtdRefri <= 62 OR $qtdRefri >= 68) {
+		return 0;
+	}
+	if (62 < $qtdRefri AND $qtdRefri < 64) {
+		return ($qtdRefri - 62) / (64 - 62);
+	}
+	if (64 <= $qtdRefri AND $qtdRefri <= 66) {
+		return 1;
+	}
+	if (56 < $qtdRefri AND $qtdRefri < 58) {
+		return (68 - $qtdRefri) / (68 - 66);
+	} else {
+		return 0;
+	}
+}
+echo gPepsiSuave(60). ' ';
+
+function gPepsiFraca ($qtdRefri){
+	if($qtdRefri <= 66 OR $qtdRefri >70){
+		return 0;
+	}
+	if (66 < $qtdRefri AND $qtdRefri >68) {
+		return ($qtdRefri-66)/(68-66);
+	}
+	if (68 <= $qtdRefri and $qtdRefri < 70) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+echo gPepsiFraca(67). ' ';
+
+function  gRunForte($qtdRun) {
+	if ($qtdRun <= 23 || $qtdRun > 30) {
+		return 0;
+	}
+	if (23 < $qtdRun && $qtdRun < 28) {
+		return ($qtdRun - 23) / (28 - 23);
+	}
+	if (28 <= $qtdRun && $qtdRun <= 30) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+echo gRunForte(26). ' ';
+
+function  gRunSuave($qtdRun) {
+	if ($qtdRun <= 15 || $qtdRun >= 27) {
+		return 0;
+	}
+	if (15 < $qtdRun && $qtdRun < 20) {
+		return ($qtdRun - 15) / (20 - 15);
+	}
+	if (20 <= $qtdRun && $qtdRun <= 25) {
+		return 1;
+	}
+	if (25 < $qtdRun && $qtdRun < 27) {
+		return (27-$qtdRun) / (27 - 25);
+	} else {
+		return 0;
+	}
+}
+echo gRunSuave(26). ' ';
+
+function gRunFraco($qtdRun) {
+	if ($qtdRun < 10 || $qtdRun >= 20) {
+		return 0;
+	}
+	if (10 <= $qtdRun && $qtdRun <= 15) {
+		return 1;
+	}
+	if (15 < $qtdRun && $qtdRun < 20) {
+		return (20 - $qtdRun) / (20 - 15);
+	} else {
+		return 0;
+	}
+}
+echo gRunFraco(17). ' ';
+
+//GELO
+function gGelo($qtdGelo) {
+	if ($qtdGelo == 20) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
 ?>
