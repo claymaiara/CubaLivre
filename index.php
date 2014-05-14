96a573b45fcba2b284933ca3b6a94c778dceda7c
Clay Maiara <clay_maiara@hotmail.com>
2014-05-14 09:55:23 -0300

    Criação de uma página só para conter o cabeçalho e assim fazer reaproveitamento de codigo

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Home</title>
-<link rel="stylesheet" href="css/bootstrap.css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-	<div class="container-fluid">
-		<nav class="navbar navbar-inverse" role="navigation">
-			<p class="navbar-text">Seja Bem Vindo</p>
-		</nav>
+<?php  include_once 'cabecalho.php'; ?>
 		<div class="jumbotron">
 			<h1>Cuba Livre</h1>
 			<p>Vamos montar o seu drink da maneira que desejar :)</p>
